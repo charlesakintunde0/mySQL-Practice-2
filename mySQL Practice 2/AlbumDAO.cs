fdb427ac13f667d15018964266d0984192d2e914
ciaki <ciaki@GIT>
2022-10-24 04:03:57 -0300

    Add project files.

---
+++ mySQL Practice 2/AlbumDAO.cs
@@ -0,0 +1,104 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mySQL_Practice_2
+{
+    internal class AlbumDAO
+    {
+        String connectionString = "datasource=localhost;port=3306;username=root;password=root;database=music;";
+
+
+
+
+        public List<Album> getAllAlbums()
+        { 
+            List<Album> returnThese = new List<Album>();
+
+            // connect to mysql server
+            MySqlConnection connection = new MySqlConnection
+                (connectionString);
+            connection.Open();
+
+            //define the sql statement to fetch all albums
+            MySqlCommand command = new MySqlCommand("SELECT ID,ALBUM_TITLE,ARTIST,YEAR,IMAGE_NAME,DESCRIPTION FROM ALBUM", connection);
+
+            using (MySqlDataReader reader = command.ExecuteReader())
+            { 
+                while(reader.Read())
+                {
+                    Album a = new Album()
+                    {
+                        ID = reader.GetInt32(0),
+                        AlbumName = reader.GetString(1),
+                        ArtistName = reader.GetString(2),
+                        Year = reader.GetInt32(3),
+                        ImageURL = reader.GetString(4),
+                        Description = reader.GetString(5)
+                    };
+
+
+                    returnThese.Add(a);
+                }
+            }
+
+            connection.Close();
+
+
+            return returnThese;
+        }
+
+        public List<Album> searchTitles(String searchTerm)
+        {
+            List<Album> returnThese = new List<Album>();
+
+            // connect to mysql server
+            MySqlConnection connection = new MySqlConnection
+                (connectionString);
+            connection.Open();
+
+            String searchWildPhrase = "%" + searchTerm + "%";
+            //define the sql statement to fetch all albums
+            MySqlCommand command = new MySqlCommand
+                ();
+
+            command.CommandText = "SELECT ID,ALBUM_TITLE,ARTIST,YEAR," +
+                "IMAGE_NAME," +
+                "DESCRIPTION FROM ALBUM WHERE " +
+                "ALBUM_TITLE LIKE @search";
+
+            command.Parameters.AddWithValue("@search", searchWildPhrase);
+            command.Connection = connection;
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Album a = new Album()
+                    {
+                        ID = reader.GetInt32(0),
+                        AlbumName = reader.GetString(1),
+                        ArtistName = reader.GetString(2),
+                        Year = reader.GetInt32(3),
+                        ImageURL = reader.GetString(4),
+                        Description = reader.GetString(5)
+                    };
+
+
+                    returnThese.Add(a);
+                }
+            }
+
+            connection.Close();
+
+
+            return returnThese;
+        }
+    }
+
+
+   
+}
+
---
+++ mySQL Practice 2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mySQL_Practice_2
+{
+    public partial class Form1 : Form
+    {
+
+        BindingSource albumBindingSource = new BindingSource();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadButtonClicked(object sender, EventArgs e)
+        {
+            AlbumDAO albumDAO = new AlbumDAO();
+
+
+
+
+
+            // connect the list to the grid view control
+
+            albumBindingSource.DataSource = albumDAO.getAllAlbums();
+
+            dataGridView1.DataSource = albumBindingSource;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButtonClicked(object sender, EventArgs e)
+        {
+            AlbumDAO albumDAO = new AlbumDAO();
+
+
+
+
+
+            // connect the list to the grid view control
+
+            albumBindingSource.DataSource = albumDAO.searchTitles(textBox1.Text);
+
+            dataGridView1.DataSource = albumBindingSource;
+        }
+    }
+}
